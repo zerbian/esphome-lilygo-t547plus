92eed6562b9ab911ed563c0d3c4124b7c59a61c8
zerbian <38952981+zerbian@users.noreply.github.com>
2025-02-15 13:33:48 +0100

    invert

--- components/t547/t547.cpp
+++ components/t547/t547.cpp
@@ -48,7 +48,7 @@ void HOT T547::draw_absolute_pixel_internal(int x, int y, Color color) {
     uint8_t gs = 0;
   // uint8_t gs = ((color.red * 2126 / 10000) + (color.green * 7152 / 10000) + (color.blue * 722 / 10000));
   if (color.red == color.green && color.green == color.blue) {
-    uint8_t gs = color.red;
+    uint8_t gs = 255 - color.red;
   }
   epd_draw_pixel(x, y, gs, this->buffer_);
 }
