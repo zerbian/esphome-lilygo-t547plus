e3b02e30d84f443f317f0b0369dad2814bdb1103
zerbian <38952981+zerbian@users.noreply.github.com>
2025-03-07 22:07:54 +0100

    Update t547.cpp

--- components/t547/t547.cpp
+++ components/t547/t547.cpp
@@ -46,7 +46,7 @@ void HOT T547::draw_absolute_pixel_internal(int x, int y, Color color) {
   if (x >= this->get_width_internal() || y >= this->get_height_internal() || x < 0 || y < 0)
     return;
   
-  uint8_t gs = ((color.red * 2126 / 10000) + (color.green * 7152 / 10000) + (color.blue * 722 / 10000));
+  uint8_t gs = 1 - ((color.red * 2126 / 10000) + (color.green * 7152 / 10000) + (color.blue * 722 / 10000));
   epd_draw_pixel(x, y, gs, this->buffer_);
 }
 
