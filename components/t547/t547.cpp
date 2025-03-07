8d68e60a609c47919ff7e4c3b6087548a2df8281
zerbian <38952981+zerbian@users.noreply.github.com>
2025-03-07 23:12:05 +0100

    Update t547.cpp

--- components/t547/t547.cpp
+++ components/t547/t547.cpp
@@ -50,8 +50,8 @@ void HOT T547::draw_absolute_pixel_internal(int x, int y, Color color) {
   else if (y < 200) gs = (color.red * 2126 + color.green * 7152 + color.blue * 722) / 10000;
   else if (y < 300) {
     gs = (color.red * 2126 + color.green * 7152 + color.blue * 722) / 10000;
-    float t = (gs / 255.0f) * (gs / 255.0f) / 255
-    gs = static_cast<uint8_t>(t);
+    uint16_t t = gs *gs;
+    gs = t / 255;
   }
   else gs = color.blue;
   //uint8_t gs = 1 - ((color.red * 2126 / 10000) + (color.green * 7152 / 10000) + (color.blue * 722 / 10000));
